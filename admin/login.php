2583a85ef855130ad2aaf42c0ad63cae8cd5cd89
Aminulloh Zaqi <aminulloh.zaqi@gmail.com>
2021-12-26 10:48:00 +0700

    update login layout

--- admin/login.php
+++ admin/login.php
@@ -10,43 +10,41 @@
 </head>
 
 <body class="my-login-page">
-		<div class="container">
-			<div class="row justify-content-center">
-				<div class="card-wrapper">
-					<div class="brand">
-						<img src="../Assets/img/logo_bmkg.png" alt="bmkg">
+	<div class="container">
+		<div class="row justify-content-center">
+			<div class="card-wrapper">
+				<div class="brand">
+					<img src="../Assets/img/logo_bmkg.png" alt="bmkg">
+				</div>
+				<div class="card fat">
+					<div class="card-body">
+						<h4 class="card-title">Login</h4>
+						<form method="POST" action="<?php echo $_SERVER["PHP_SELF"];?>" class="my-login-validation" id="needs-validation" novalidate>
+							<div class="form-group">
+								<label for="username">Username</label>
+								<input id="username" type="text" class="form-control" name="username" value="" required autofocus>
+								<div class="invalid-feedback">
+								</div>
+							</div>
+							<div class="form-group">
+								<label for="password">Password
+								</label>
+								<input id="password" type="password" class="form-control" name="password" required data-eye>
+							    <div class="invalid-feedback">
+							    	Username or Password is invalid
+						    	</div>
+							</div>
+							<div class="form-group">
+								<button type="submit" name="Submit" onclick="myFunction()" class="btn btn-primary btn-block form-control">
+									Login
+								</button>
+							</div>
+						</form>
 					</div>
-					<div class="card fat">
-						<div class="card-body">
-							<h4 class="card-title">Login</h4>
-							<form method="POST" action="<?php echo $_SERVER["PHP_SELF"];?>" class="my-login-validation" id="needs-validation" novalidate>
-								<div class="form-group">
-									<label for="username">Username</label>
-									<input id="username" type="text" class="form-control" name="username" value="" required autofocus>
-									<div class="invalid-feedback">
-									</div>
-								</div>
-
-								<div class="form-group">
-									<label for="password">Password
-									</label>
-									<input id="password" type="password" class="form-control" name="password" required data-eye>
-								    <div class="invalid-feedback">
-								    	Username or Password is invalid
-							    	</div>
-								</div>
-								<div class="form-group">
-									<button type="submit" name="Submit" onclick="myFunction()" class="btn btn-primary btn-block form-control">
-										Login
-									</button>
-								</div>
-							</form>
-						</div>
-					</div>
-					
 				</div>
 			</div>
 		</div>
+	</div>
 
 	<script src="../Assets/js/popper.min.js"></script>
     <script src="../Assets/js/bootstrap@5.1.3.js"></script>
